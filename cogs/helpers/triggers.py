b49eed053d1942419f9c2f3c8c08e12b60871c23
Arthur <jovart.arthur@gmail.com>
2019-06-18 18:41:51 +0200

    ✨ Sexbots are now using older accounts, this is an autotriggers fix.

--- cogs/helpers/triggers.py
+++ cogs/helpers/triggers.py
@@ -98,10 +98,10 @@ class SexBots(AutoTrigger):
         self.autotrigger_dbname = "sexbots"
 
     async def check(self):
-        assert await message_contains_x_of(self.message, 1, ["privatepage.vip", "nakedphotos.club", "viewc.site", "My naked photos", "My 18+ photos"])
+        assert await message_contains_x_of(self.message, 1, ["privatepage.vip", "nakedphotos.club", "viewc.site", "My naked photos", "My 18+ photos", "Awesome Gift of the Day"])
 
         assert await member_joined_x_days_ago(self.message.author, x=2)
-        assert await user_created_x_days_ago(self.message.author, x=3)
+        assert await user_created_x_days_ago(self.message.author, x=7)
         assert not await user_have_nitro(self.message.author)
 
         return True
